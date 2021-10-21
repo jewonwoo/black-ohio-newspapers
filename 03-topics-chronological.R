c2b365dc2439ad4d982e69431e8952779ee73b5a
Jason Heppler <jason.heppler@gmail.com>
2021-10-21 11:09:17 -0500

    Stacked bar charts

--- 03-topics-chronological.R
+++ 03-topics-chronological.R
@@ -4,9 +4,15 @@ library(ggthemes)
 
 # Chronological change of topics
 
+# Small multiples
 ggplot(topics, aes(x = decade, y = frequency)) +
   geom_bar(stat="identity") +
-  facet_wrap(~topic) +
-  theme_wsj()
+  facet_wrap(~topic, ncol = 7) +
+  theme_minimal() 
 
 ggplotly(ggplot2::last_plot())
+
+# Stacked
+ggplot(topics, aes(x = decade, y = frequency, fill = topic)) +
+  geom_bar(stat="identity", color="black") +
+  scale_fill_brewer(type = "qual", palette = "Set3")
--- 04-advertisements-chronological.R
+++ 04-advertisements-chronological.R
@@ -4,9 +4,14 @@ library(plotly)
 
 # Chronological change of advertisements
 
+# Small multiples
 ggplot(advertisements, aes(x = decade, y = frequency)) +
   geom_bar(stat="identity") +
-  facet_wrap(~category) +
-  theme_wsj()
+  facet_wrap(~category, ncol = 6) +
+  theme_minimal()
 
 ggplotly(ggplot2::last_plot())
+
+# Stacked
+ggplot(advertisements, aes(x = decade, y = frequency, fill = category)) +
+  geom_bar(stat="identity") 
