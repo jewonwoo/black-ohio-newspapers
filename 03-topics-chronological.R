69d602801a71fbd9840c5bc199be115ca09f4f55
Jason Heppler <jason.heppler@gmail.com>
2021-11-17 11:35:12 -0600

    Layout and design tweaks for readability

--- 03-topics-chronological.R
+++ 03-topics-chronological.R
@@ -7,11 +7,13 @@ library(ggthemes)
 # Small multiples
 top <- ggplot(topics, aes(x = decade, y = frequency)) +
   geom_bar(stat="identity") +
-  facet_wrap(~topic, ncol = 7) +
-  theme_minimal() 
+  facet_wrap(~topic, ncol = 2) +
+  theme_minimal() +
+  theme(legend.position="none") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 top
 
-top_plotly <- ggplotly(top)
+top_plotly <- ggplotly(top, width = 800, height = 800)
 
 ## Export as HTML and PNG
 htmlwidgets::saveWidget(as_widget(top_plotly), "html/topics.html")
@@ -20,10 +22,11 @@ ggsave(top, device = "png", dpi = 300, path = "png/topics.png")
 # Stacked
 top_stacked <- ggplot(topics, aes(x = decade, y = frequency, fill = topic)) +
   geom_bar(stat="identity", color="black") +
-  scale_fill_brewer(type = "qual", palette = "Set3")
+  scale_fill_brewer(type = "qual", palette = "Set3") +
+  theme(legend.position="none")
 top_stacked
 
-stacked_plotly <- ggplotly(top_stacked)
+stacked_plotly <- ggplotly(top_stacked, width = 800, height = 800)
 
 ## Export as HTML and PNG
 htmlwidgets::saveWidget(as_widget(stacked_plotly), "html/topics_stacked.html")
--- 04-advertisements-chronological.R
+++ 04-advertisements-chronological.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(ggthemes)
 library(plotly)
+library(viridis)
+library(treemap)
+library(d3Tree)
 
 # Chronological change of advertisements
 
@@ -8,10 +11,12 @@ library(plotly)
 ads_multiple <- ggplot(advertisements, aes(x = decade, y = frequency)) +
   geom_bar(stat="identity") +
   facet_wrap(~category, ncol = 6) +
-  theme_minimal()
+  theme_minimal() +
+  theme(legend.position="none") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ads_multiple
 
-ads_plotly <- ggplotly(ads_multiple)
+ads_plotly <- ggplotly(ads_multiple, width = 800, height = 800)
 
 ## Export as HTML and PNG
 htmlwidgets::saveWidget(as_widget(ads_plotly), "html/ads.html")
@@ -19,12 +24,27 @@ ggsave(ads_multiple, device = "png", dpi = 300, path = "png/ads.png")
 
 # Stacked
 ads_stacked <- ggplot(advertisements, aes(x = decade, y = frequency, fill = category)) +
-  geom_bar(stat="identity") 
+  geom_bar(stat="identity") +
+  theme(legend.position="none")
 ads_stacked
 
-ads_stacked_plotly <- ggplotly(ads_stacked)
+ads_stacked_plotly <- ggplotly(ads_stacked, width = 800, height = 800)
 
 ## Export as HTML and PNG
 htmlwidgets::saveWidget(as_widget(ads_stacked_plotly), "html/ads_stacked.html")
 ggsave(ads_stacked, device = "png", dpi = 300, path = "png/ads_stacked.png")
 
+# Treemap
+ads_tree <- treemap(
+  advertisements, 
+  index=c("decade","category"),
+  vSize="frequency",
+  type="index",
+  palette = "Set2",
+  bg.labels=c("white"),
+  align.labels=list(
+    c("left", "top"),
+    c("right", "bottom")
+  )
+) 
+ads_tree
